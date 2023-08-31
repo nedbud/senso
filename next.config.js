ccb607d7bc2d19bda3e65df14d8344cb62332290
xxrafidxx <rafidahsanofficial@gmail.com>
2023-08-31 16:41:55 +0600

    add: prod

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    mySecret: "secret",
-    // Network IP address you want to bind to
-    HOST: "192.168.0.103",
-    // Optional: Port number you want to use, default is 3000
-    PORT: 3000,
-  },
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  //   serverRuntimeConfig: {
+  //     // Will only be available on the server side
+  //     mySecret: "secret",
+  //     // Network IP address you want to bind to
+  //     HOST: "192.168.0.103",
+  //     // Optional: Port number you want to use, default is 3000
+  //     PORT: 3000,
+  //   },
 };
 
 module.exports = nextConfig;
